ab12fc276d8dfd1602cd1aa3a09b45d2c9323e67
Vikt0r <vbachmanov@mail.ru>
2022-08-03 16:29:44 +0300

    Establish the app's state

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,51 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
   <AddLocation />
+  <div v-if="isInitialOpening">There is initial opening</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-//import HelloWorld from "./components/HelloWorld.vue";
+<script setup lang="ts">
+// import { defineComponent } from "vue";
+// //import HelloWorld from "./components/HelloWorld.vue";
 import AddLocation from "./components/AddLocation.vue";
 
-export default defineComponent({
-  name: "App",
-  components: {
-    //HelloWorld,
-    AddLocation,
-  },
+// export default defineComponent({
+//   name: "App",
+//   components: {
+//     //HelloWorld,
+//     AddLocation,
+//   },
+
+// });
+
+import { reactive, ref, computed } from "vue";
+import type { Ref } from "vue";
+//import { LOCAL_CONFIG } from "./ts/constants";
+
+console.log("Setup App");
+
+const LOCAL_CONFIG = "weather_widget_vb";
+
+addEventListener("beforeunload", () => {
+  localStorage.setItem(LOCAL_CONFIG, JSON.stringify(locations));
 });
+
+interface Location {
+  city: string;
+  fetched_at: number;
+}
+
+const locations: Array<Location> = reactive([]);
+
+//type Mode = 'weather' | 'settings';
+
+const isInitialOpening = computed(
+  () => localStorage.getItem(LOCAL_CONFIG) === null
+);
+
+const mode: Ref<"weather" | "settings"> = ref(
+  isInitialOpening ? "settings" : "weather"
+);
 </script>
 
 <style lang="scss">
