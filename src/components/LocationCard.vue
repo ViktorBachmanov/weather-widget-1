bda0948a06adff468ef32654cd867c944c00250a
Vikt0r <vbachmanov@mail.ru>
2022-08-07 20:20:19 +0300

    Add pressure info

--- src/components/LocationCard.vue
+++ src/components/LocationCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h4>{{ data.name }}</h4>
-    <div class="icon-n-temp">
+    <div class="row">
       <img
         alt="weather"
         :src="`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`"
@@ -9,13 +9,23 @@
       <span class="temp">{{ data.main.temp }}&deg;C</span>
     </div>
     Feels like {{ data.main.feels_like }}&deg;C
-    <div class="wind">
-      <img
-        alt="wind_dir"
-        src="@/assets/arrow_dark.svg"
-        :style="{ transform: `rotate(${data.wind.deg}deg)` }"
-      />
-      <span class="wind-speed"> {{ data.wind.speed }} m/s </span>
+    <div class="row">
+      <span class="param wind">
+        <img
+          alt="wind_dir"
+          src="@/assets/arrow_dark.svg"
+          :style="{ transform: `rotate(${data.wind.deg}deg)` }"
+        />
+        <span class="wind-speed"> {{ data.wind.speed }} m/s </span>
+      </span>
+      <span class="param">
+        <img
+          alt="pressure"
+          src="@/assets/pressure.png"
+          style="width: 1.25em; margin-right: 0.25em"
+        />
+        <span> {{ data.main.pressure }} hPa </span>
+      </span>
     </div>
   </div>
 </template>
@@ -37,18 +47,21 @@ const props = defineProps<Props>();
   flex-direction: column;
   align-items: center;
 }
-.icon-n-temp {
+.row {
+  display: flex;
+  align-items: center;
+}
+.param {
   display: flex;
   align-items: center;
 }
 .temp {
   font-size: x-large;
 }
-
 .wind {
-  display: flex;
-  align-items: center;
+  margin-right: 1em;
 }
+
 .wind-speed {
   margin: 0 0.5em;
 }
